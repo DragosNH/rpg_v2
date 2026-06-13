e40aa93d14737ec6f904e9799ff11b6615c854a7
Dragoş <edragos33@gmail.com>
2026-06-13 16:27:21 +0200

    added some creatures

---
+++ creatures.js
@@ -0,0 +1,18 @@
+export const creatures = [
+    {
+        id: 1,
+        name: "Wolf",
+        level: 1,
+        hp: 20,
+        xp: 5,
+        dammage_dealing: Math.floor((Math.random() * 5) + 2),
+    },
+    {
+        id: 2,
+        name: "Wolf Boss",
+        level: 3,
+        hp: 30,
+        hp: 10,
+        dammage_dealing: Math.floor((Math.random() * 8) + 3),
+    }
+];
---
+++ script.js
@@ -0,0 +1,4 @@
+import { creatures } from "./creatures.js";
+
+const creatureName = creatures.find(creature => creature.id === 1).name;
+
