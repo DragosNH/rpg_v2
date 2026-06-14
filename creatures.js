78916952f9359b2a49e4f78829e07dc1c37cc0d7
Dragoş <edragos33@gmail.com>
2026-06-14 18:37:01 +0200

    small changes

--- creatures.js
+++ creatures.js
@@ -11,8 +11,8 @@ export const creatures = [
         id: 2,
         name: "Wolf Boss",
         level: 3,
-        hp: 30,
-        hp: 10,
+        hp: 50,
+        xp: 10,
         dammage_dealing: Math.floor((Math.random() * 8) + 3),
     }
 ];
