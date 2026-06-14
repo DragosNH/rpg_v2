4ae597bd7319762eaf65664fca6ec8e1ad3ded51
Dragoş <edragos33@gmail.com>
2026-06-14 18:44:22 +0200

    added hero name

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 import { creatures } from "./creatures.js";
 
+const main = document.querySelector("main");
 const submitName = document.querySelector(".submitName");
 const inputName = document.querySelector(".inputName");
 
 const creatureName = creatures.find(creature => creature.id === 1).name;
+
+submitName.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let heroName = inputName.value;
+
+    const heroContainer = `<h2>${heroName}</h2>`;
+
+    inputName.value = "";
+    main.insertAdjacentHTML('beforeend', heroContainer)
+})
 
