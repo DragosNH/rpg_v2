39b495ad337dbff96e22f04a1dcff08e9a6799bf
Dragoş <edragos33@gmail.com>
2026-06-14 18:36:13 +0200

    declared variables

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 import { creatures } from "./creatures.js";
 
+const submitName = document.querySelector(".submitName");
+const inputName = document.querySelector(".inputName");
+
 const creatureName = creatures.find(creature => creature.id === 1).name;
 
